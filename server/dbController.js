91bb0470c119ef2a2e0d88600832f28492e2a875
Mirko Rajovic <rajovicmirko@gmail.com>
2020-04-29 17:40:18 +0200

    Heroku added some dependencies

--- server/dbController.js
+++ server/dbController.js
@@ -4,7 +4,7 @@ const dbName = 'myFirstDb';
 const dbPassword = 'Mirkasin.12345';
 const dbConnUrl = `mongodb+srv://rajovicmirko:${dbPassword}@cluster0-folf1.mongodb.net/test?retryWrites=true&w=majority`;
 
-mongoose.connect(dbConnUrl, { dbName: dbName, useNewUrlParser: true })
+mongoose.connect(dbConnUrl, { dbName: dbName, useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log(`Connected to ${dbName} db...`));
 
 module.exports = mongoose;
